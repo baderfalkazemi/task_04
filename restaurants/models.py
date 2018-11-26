8c353254e145834db02dc6ded06b6060269aae8a
baderfalkazemi <bader.alkazemi@gmail.com>
2018-11-26 20:33:58 +0300

    tas4 submit

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.CharField(max_length=200)
+	opening_time = models.TimeField(default="01:45:33")
+	closing_time = models.TimeField(default="01:45:33")
+
+	def __str__(self):
+		return self.name
